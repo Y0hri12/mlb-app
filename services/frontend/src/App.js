ccd17ce8e7fd44fcff65b7cdd6d2e320e84c5fc1
yori.kai <yori.kai@optim.co.jp>
2023-10-10 11:37:18 +0900

    APIの呼び出し確認 (#6)

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState();
+
+  // Go -> React
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await axios.get("/api");
+        setData(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getData();
+  }, []);
+
+  // React -> Go
+  const createPost = () => {
+    axios.post("/api/message", { message: "This message from React" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, World</h1>
+      <div>Recieved -&gt; {data ? data.message : "No data"}</div>
+      <br />
+      <br />
+      <button onClick={createPost}>Create POST</button>
     </div>
   );
 }
